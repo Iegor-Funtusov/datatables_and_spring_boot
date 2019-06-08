ba4cf85d168cc5afcce74dcf14dd9aed2e886fca
Dmitriy Lapko <dla.own@gmail.com>
2019-06-08 07:33:12 +0300

    Reformat code and fix warning in unchecked cast

--- src/main/java/org/springframework/data/jpa/datatables/easy/web/EasyDatatablesListController.java
+++ src/main/java/org/springframework/data/jpa/datatables/easy/web/EasyDatatablesListController.java
@@ -17,133 +17,138 @@ import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import org.springframework.data.jpa.datatables.mapping.Order;
 import org.springframework.data.jpa.datatables.mapping.Search;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.ui.Model;
 import org.springframework.web.context.request.WebRequest;
 
 public abstract class EasyDatatablesListController<T> {
 
-    private static final String FILTER_PARAM_PREFIX = "filter_";
+	private static final String FILTER_PARAM_PREFIX = "filter_";
 	private static final int DEFAULT_PAGE_SIZE = 10;
 
-    @Autowired
-    protected SessionData sessionData;
+	@Autowired
+	protected SessionData sessionData;
 
-    protected abstract String getListCode();
-    protected abstract Map<String, List<Enum<?>>> getListEnumsField();
-    protected abstract DataTablesRepository<T, Long> getDataTableRepository();
+	protected abstract String getListCode();
 
-    protected String list(Model model, WebRequest webRequest) {
-        PageData pageData = updatePageData(webRequest);
+	protected abstract Map<String, List<Enum<?>>> getListEnumsField();
 
-        DataTablesOutput<T> dto;
-        DataTablesData<T> data = toDataTablesInput(pageData);
-        if (data.getSpecification() == null) {
-            dto = getDataTableRepository().findAll(data.getInput());
-        } else {
-            dto = getDataTableRepository().findAll(data.getInput(), data.getSpecification());
-        }
+	protected abstract DataTablesRepository<T, Long> getDataTableRepository();
 
-        DataTablesUtil.updatePageData(pageData, dto.getRecordsFiltered());
-        model.addAttribute(getListCode() + "List", dto.getData());
-        model.addAttribute(getListCode() + "Page", pageData);
-        if (!getListEnumsField().isEmpty()) {
-            model.addAttribute("enums", getListEnumsField());
-        }
-        return "/" + getListCode() + "/list";
-    }
+	protected String list(Model model, WebRequest webRequest) {
+		PageData pageData = updatePageData(webRequest);
 
-    private PageData updatePageData(WebRequest webRequest) {
-        PageData pd = getPageData();
-        if (webRequest.getParameter("clear") != null) {
-        	pd.clear();
-        }
-        
-        String pageParam = webRequest.getParameter("page");
-        if (StringUtils.isNotBlank(pageParam)) {
-            int page = Integer.parseInt(pageParam);
-            pd.setPage(page);
-        }
-        String sizeParam = webRequest.getParameter("size");
-        if (StringUtils.isNotBlank(sizeParam)) {
-            int size = Integer.parseInt(sizeParam);
-            pd.setSize(size);
-        }
+		DataTablesOutput<T> dto;
+		DataTablesData<T> data = toDataTablesInput(pageData);
+		if (data.getSpecification() == null) {
+			dto = getDataTableRepository().findAll(data.getInput());
+		} else {
+			dto = getDataTableRepository().findAll(data.getInput(), data.getSpecification());
+		}
 
-        String orderParam = webRequest.getParameter("order");
-        if (StringUtils.isNotBlank(sizeParam)) {
-            pd.setOrder(orderParam);
-        }
-        
-        Map<String, String[]> parameterMap = webRequest.getParameterMap();
+		DataTablesUtil.updatePageData(pageData, dto.getRecordsFiltered());
+		model.addAttribute(getListCode() + "List", dto.getData());
+		model.addAttribute(getListCode() + "Page", pageData);
+		if (!getListEnumsField().isEmpty()) {
+			model.addAttribute("enums", getListEnumsField());
+		}
+		return "/" + getListCode() + "/list";
+	}
+
+	private PageData updatePageData(WebRequest webRequest) {
+		PageData pd = getPageData();
+		if (webRequest.getParameter("clear") != null) {
+			pd.clear();
+		}
+
+		String pageParam = webRequest.getParameter("page");
+		if (StringUtils.isNotBlank(pageParam)) {
+			int page = Integer.parseInt(pageParam);
+			pd.setPage(page);
+		}
+		String sizeParam = webRequest.getParameter("size");
+		if (StringUtils.isNotBlank(sizeParam)) {
+			int size = Integer.parseInt(sizeParam);
+			pd.setSize(size);
+		}
+
+		String orderParam = webRequest.getParameter("order");
+		if (StringUtils.isNotBlank(sizeParam)) {
+			pd.setOrder(orderParam);
+		}
+
+		Map<String, String[]> parameterMap = webRequest.getParameterMap();
 		for (String paramName : parameterMap.keySet()) {
 			if (paramName.startsWith(FILTER_PARAM_PREFIX)) {
-				pd.addFilterValue(paramName.substring(FILTER_PARAM_PREFIX.length()),  parameterMap.get(paramName));
+				pd.addFilterValue(paramName.substring(FILTER_PARAM_PREFIX.length()), parameterMap.get(paramName));
 			}
 		}
-        return pd;
-    }
+		return pd;
+	}
 
-    private PageData getPageData() {
-        return sessionData.getOrCreatePageData(this.getClass(), DEFAULT_PAGE_SIZE);
-    }
+	private PageData getPageData() {
+		return sessionData.getOrCreatePageData(this.getClass(), DEFAULT_PAGE_SIZE);
+	}
 
-    private DataTablesData<T> toDataTablesInput(PageData pd) {
-        DataTablesInput i = new DataTablesInput();
-        Map<String, String> specificValueMap = new HashMap<>();
+	private DataTablesData<T> toDataTablesInput(PageData pd) {
+		DataTablesInput i = new DataTablesInput();
+		Map<String, String> specificValueMap = new HashMap<>();
 
-        i.setLength(pd.getSize());
-        i.setStart(((pd.getPage() - 1) * pd.getSize()));
+		i.setLength(pd.getSize());
+		i.setStart(((pd.getPage() - 1) * pd.getSize()));
 
-        List<Column> columns = new ArrayList<>();
+		List<Column> columns = new ArrayList<>();
 
-        if (StringUtils.isNotBlank(pd.getOrder())) {
-            String[] orderSplit = pd.getOrder().split("_");
-            if (orderSplit.length == 2 && "asc,desc".contains(orderSplit[1])) {
-                String fieldName = orderSplit[0];
-                String ascDesc = orderSplit[1];
-                List<Order> orders = Collections.singletonList(new Order(0, ascDesc));
-                columns.add(initColumns(fieldName, ""));
-                i.setOrder(orders);
-                i.setColumns(columns);
-            }
-        }
+		if (StringUtils.isNotBlank(pd.getOrder())) {
+			String[] orderSplit = pd.getOrder().split("_");
+			if (orderSplit.length == 2 && "asc,desc".contains(orderSplit[1])) {
+				String fieldName = orderSplit[0];
+				String ascDesc = orderSplit[1];
+				List<Order> orders = Collections.singletonList(new Order(0, ascDesc));
+				columns.add(initColumns(fieldName, ""));
+				i.setOrder(orders);
+				i.setColumns(columns);
+			}
+		}
 
-        if (pd.getFilterMap() != null) {
-            for (Map.Entry<String, String> entry : pd.getFilterMap().entrySet()) {
-                columns.add(initColumns(entry.getKey(), entry.getValue()));
-            }
-            if (CollectionUtils.isNotEmpty(columns)) {
-                for (Column column : columns) {
-                    if (column.getData().contains(".") && StringUtils.isNotBlank(column.getSearch().getValue())) {
-                        Search search = column.getSearch();
-                        String field = column.getData();
-                        String fieldName = field.substring(0, field.indexOf("."));
-                        specificValueMap.put(fieldName, search.getValue());
-                        search.setValue("");
-                    }
-                }
-                i.setColumns(columns);
-            }
-        }
+		if (pd.getFilterMap() != null) {
+			for (Map.Entry<String, String> entry : pd.getFilterMap().entrySet()) {
+				columns.add(initColumns(entry.getKey(), entry.getValue()));
+			}
+			if (CollectionUtils.isNotEmpty(columns)) {
+				for (Column column : columns) {
+					if (column.getData().contains(".") && StringUtils.isNotBlank(column.getSearch().getValue())) {
+						Search search = column.getSearch();
+						String field = column.getData();
+						String fieldName = field.substring(0, field.indexOf("."));
+						specificValueMap.put(fieldName, search.getValue());
+						search.setValue("");
+					}
+				}
+				i.setColumns(columns);
+			}
+		}
 
-        DataTablesData<T> data = new DataTablesData<>();
-        data.setInput(i);
-        if (!specificValueMap.isEmpty()) {
-            data.setSpecification(
-                    new SpecificationUtil<T>().generateFinishSpecification(
-                            specificValueMap, (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]));
-        }
+		DataTablesData<T> data = new DataTablesData<>();
+		data.setInput(i);
+		if (!specificValueMap.isEmpty()) {
+			ParameterizedType superclass = (ParameterizedType) getClass().getGenericSuperclass();
+			@SuppressWarnings("unchecked")
+			Class<T> entityClass = (Class<T>) superclass.getActualTypeArguments()[0];
+			Specification<T> spec = new SpecificationUtil<T>().generateFinishSpecification(specificValueMap, entityClass);
+			data.setSpecification(spec);
+		}
 
-        return data;
-    }
+		return data;
+	}
 
-    private Column initColumns(String columnName, String searchValue) {
-        Column column = new Column();
-        column.setSearchable(true);
-        column.setOrderable(true);
-        column.setData(columnName);
-        column.setName("");
-        column.setSearch(new Search(searchValue, false));
-        return column;
-    }
+	private Column initColumns(String columnName, String searchValue) {
+		Column column = new Column();
+		column.setSearchable(true);
+		column.setOrderable(true);
+		column.setData(columnName);
+		column.setName("");
+		column.setSearch(new Search(searchValue, false));
+		return column;
+	}
 }
